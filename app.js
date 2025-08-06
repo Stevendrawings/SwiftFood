0239d097be762f75d5f35c2866dd2099e5ea162e
Stevens <stevenchauvets@gmail.com>
2025-08-06 18:24:36 +0200

    Ajout des nombres dans le block

--- app.js
+++ app.js
@@ -3,30 +3,36 @@ let tab = [];
 tab = Array(6).fill('')
 
 const btn = document.querySelectorAll(".block")
+const p = document.querySelectorAll("p")
 
 for(let i = 0; i < btn.length; i = i + 1){
 
     let count = 0;
     let indice = 1;
 
-    btn[i].addEventListener('click', function clikcord(){
+    p[i].innerText = "0";
+
+btn[i].addEventListener('click', function clikcord(){
+
         if(indice <= 6){ 
             tab.splice(i, 1, indice)
             indice++ 
-        }  else {
+        } else {
             return false;
         } 
     
     // 0 + 1 + 2 + 3 + 4         
     const initVal = tab.reduce((acc, val) => acc + parseInt(Number(val)), count);  
 
-    if(initVal <= 6){
-        console.log("tab:", tab);
-        console.log("initVal:", initVal);
-    } else {
-        return false
-    }
+        if(initVal <= 6){
+            console.log("tab:", tab);
+            p[i].innerText = tab[i]
+            console.log("initVal:", initVal);
+        } else {
+            return false
+        }
     });
+
 }
 
 
